e848097b4719da9919ec37d093037e735f5069ea
Shiva Saini <shivrajsaini1052@gmail.com>
2026-06-25 12:33:25 +0530

    Time: 5 ms (14.3%), Space: 43.6 MB (10.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+         for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if (st.isEmpty() || ch =='(' || ch == '[' || ch== '{' )  st.push(ch);
+            else {
+                if (ch ==')' && st.peek() == '(')  st.pop();
+                else if (ch =='}' && st.peek() == '{')  st.pop();
+                else if (ch ==']' && st.peek() == '[')  st.pop();
+                else return false;
+            }
+         }
+         if (!st.isEmpty()) return false;
+         return true;
+    }
+}
