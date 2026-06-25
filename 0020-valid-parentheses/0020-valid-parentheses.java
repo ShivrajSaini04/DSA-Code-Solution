ad6a0ba7e05f65860c50e3efb6992075de96aecc
Shiva Saini <shivrajsaini1052@gmail.com>
2026-06-25 12:39:32 +0530

    Time: 4 ms (38.63%), Space: 43.5 MB (25.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
+        if (s.length()%2==1) return false ;  // odd length always not valid
          for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
             if (st.isEmpty() || ch =='(' || ch == '[' || ch== '{' )  st.push(ch);
