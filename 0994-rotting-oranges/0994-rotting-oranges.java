ff7036aa452b7f30d437f29573d3f935d69e3a59
Shiva Saini <shivrajsaini1052@gmail.com>
2026-06-27 22:43:50 +0530

    Time: 2 ms (90.73%), Space: 44.3 MB (39.93%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        Queue<int[]> q = new LinkedList<>();
+
+        int fresh = 0;
+        int minutes = 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // Add all rotten oranges
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == 2)
+                    q.offer(new int[]{i, j});
+
+                else if (grid[i][j] == 1)
+                    fresh++;
+            }
+        }
+
+        int[][] dir = {
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1}
+        };
+
+        while (!q.isEmpty() && fresh > 0) {
+
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+
+                int[] curr = q.poll();
+
+                for (int[] d : dir) {
+
+                    int nr = curr[0] + d[0];
+                    int nc = curr[1] + d[1];
+
+                    if (nr >= 0 && nr < m &&
+                        nc >= 0 && nc < n &&
+                        grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.offer(new int[]{nr, nc});
+                    }
+                }
+            }
+
+            minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+}
