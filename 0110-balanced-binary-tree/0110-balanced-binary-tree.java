2bd31890ea45ceae3d8463d39fb0825e5ab3e2eb
Shiva Saini <shivrajsaini1052@gmail.com>
2026-07-06 13:41:33 +0530

    Time: 1 ms (31.79%), Space: 45.4 MB (76.67%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    static boolean flag;
+    public boolean isBalanced(TreeNode root) {
+        if (root == null ) return true;
+        flag = true;
+        level(root);
+        return flag;
+    }
+
+     int level(TreeNode root){
+        if(root == null) return 0;
+        int left = level(root.left);
+        int right = level(root.right);
+        if (Math.abs(left - right) > 1) flag = false;
+        return 1 + Math.max(left , right);
+     }
+}
