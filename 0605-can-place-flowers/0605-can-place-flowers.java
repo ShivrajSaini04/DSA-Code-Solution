c42a2cdd1a7ad3965520a74f369d460b742a8a6c
Shiva Saini <shivrajsaini1052@gmail.com>
2026-07-22 22:58:07 +0530

    Time: 1 ms (98.73%), Space: 47.5 MB (94.48%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean canPlaceFlowers(int[] arr, int k) {
         int n = arr.length;
         if (  (k==1 && n == 1) && arr[0] == 0) return true;
-        if ( n > 1 && arr[0] == 0 && arr[1] != 1) {
-            arr[0] = 1;
-            k--;
-        }
+        // if ( n > 1 && arr[0] == 0 && arr[1] != 1) {
+        //     arr[0] = 1;
+        //     k--;
+        // }
 
-        for(int i =1; i<n-1; i++){
-            if (arr[i] == 0 && (arr[i-1] == 0 && arr[i+1] == 0)) {
+        for(int i =0; i<n-1; i++){
+            if (arr[i] == 0 && ((i==0 || arr[i-1] == 0) && arr[i+1] == 0)) {
               arr[i] = 1;
               k--;
             }
