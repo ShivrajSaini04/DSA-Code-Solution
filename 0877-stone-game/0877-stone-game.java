0440bafdf82d3cf5576593ec3438c4308e1d0323
Shiva Saini <shivrajsaini1052@gmail.com>
2026-08-02 11:16:37 +0530

    Time: 12 ms (5.73%), Space: 50 MB (5.29%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,18 @@
 class Solution {
+    Integer[][] dp;
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        dp = new Integer[n][n];
+        return solve(0,n-1,piles) > 0;
+    }
+
+    int solve(int left , int rig ,int[] piles){
+        if (left == rig) return piles[left];
+        if (dp[left][rig] != null) return dp[left][rig];
+
+        int leftStone = piles[left] - solve(left +1 ,rig,piles);
+        int rightStone = piles[rig] - solve(left ,rig - 1 ,piles);
+
+        return dp[left][rig] = Math.max(leftStone,rightStone);
     }
 }
